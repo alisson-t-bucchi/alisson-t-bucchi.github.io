49c08e975a99bdc4ffc6dd2e644dd9fed295ceff
alisson-t-bucchi <alisson.bucchi@outlook.com>
2024-09-02 22:30:26 +0100

    improving skills

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
 let menuVisible = false;
 
 function mostrarOcultarMenu() {
-    if (menuVisible) {
-        document.getElementById("nav").classList = "";
-        menuVisible = false;
-    } else {
-        document.getElementById("nav").classList = "responsive";
-        menuVisible = true;
-    }
+  if (menuVisible) {
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+  } else {
+    document.getElementById("nav").classList = "responsive";
+    menuVisible = true;
+  }
 }
 
 function seleccionar() {
-    document.getElementById("nav").classList = "";
-    menuVisible = false;
+  document.getElementById("nav").classList = "";
+  menuVisible = false;
 }
 
 function efectoHabilidades() {
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if (distancia_skills >= 300) {
-        let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("linq");
-        habilidades[1].classList.add("htmlcssjavascript");
-        habilidades[2].classList.add("aspnetmvc");
-        habilidades[3].classList.add("csharp");
-        habilidades[4].classList.add("xamarin");
-        habilidades[5].classList.add("comunication");
-        habilidades[6].classList.add("teamwork");
-        habilidades[7].classList.add("creativity");
-        habilidades[8].classList.add("dedication");
-        habilidades[9].classList.add("project");
-    }
+  var skills = document.getElementById("skills");
+  var distancia_skills =
+    window.innerHeight - skills.getBoundingClientRect().top;
+  if (distancia_skills >= 300) {
+    let habilidades = document.getElementsByClassName("progreso");
+    habilidades[0].classList.add("java");
+    habilidades[1].classList.add("javascript");
+    habilidades[2].classList.add("python");
+    habilidades[3].classList.add("selenium");
+    habilidades[4].classList.add("cypress");
+    habilidades[5].classList.add("robot framework");
+    habilidades[6].classList.add("comunication");
+    habilidades[7].classList.add("teamwork");
+    habilidades[8].classList.add("creativity");
+    habilidades[9].classList.add("dedication");
+    habilidades[10].classList.add("project management");
+  }
 }
 
-
 window.onscroll = function () {
-    efectoHabilidades();
-} 
+  efectoHabilidades();
+};
