1b4e8ebb150fa45d5992cf9c0d21a86ae0988b9d
alisson-t-bucchi <alisson.bucchi@outlook.com>
2024-11-24 13:08:32 +0000

    Changes in CV container and add some icons

---
+++ script.js
@@ -0,0 +1,40 @@
+let menuVisible = false;
+
+function showMenu() {
+  if (menuVisible) {
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+  } else {
+    document.getElementById("nav").classList = "responsive";
+    menuVisible = true;
+  }
+}
+
+function selection() {
+  document.getElementById("nav").classList = "";
+  menuVisible = false;
+}
+
+function skillsEffect() {
+  var skills = document.getElementById("skills");
+  var distancia_skills =
+    window.innerHeight - skills.getBoundingClientRect().top;
+  if (distancia_skills >= 300) {
+    let habilidades = document.getElementsByClassName("progress");
+    habilidades[0].classList.add("java");
+    habilidades[1].classList.add("javascript");
+    habilidades[2].classList.add("python");
+    habilidades[3].classList.add("selenium");
+    habilidades[4].classList.add("cypress");
+    habilidades[5].classList.add("robot");
+    habilidades[6].classList.add("comunication");
+    habilidades[7].classList.add("teamwork");
+    habilidades[8].classList.add("creativity");
+    habilidades[9].classList.add("dedication");
+    habilidades[10].classList.add("project");
+  }
+}
+
+window.onscroll = function () {
+  skillsEffect();
+};
